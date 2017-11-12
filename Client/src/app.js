9db31e53a6c0e32dc1e1abdf4a7988401d95b5a1
canhtrinh <canh.coding@gmail.com>
2017-11-11 23:26:47 -0500

    basic Client folder structure set up

---
+++ Client/src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Login from "./components/Login_Page/Login"
+
+export default class App extends React.Component {
+
+    render() {
+        return (<div>
+            <Login />
+        </div>);
+    }
+  
+}
