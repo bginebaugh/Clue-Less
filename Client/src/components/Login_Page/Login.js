c46dcc4acd6ae89856640f18940e4efc96731663
canhtrinh <canh.coding@gmail.com>
2017-11-12 10:18:27 -0500

    #9 demo button added

--- Client/src/components/Login_Page/Login.js
+++ Client/src/components/Login_Page/Login.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 export default class Login extends React.Component {
 
+    alertMe() {
+        alert("Look at me! Hello NorthRaki!");
+    }
+
     render() {
         return (<div>
-            Clue-Less Login!
+            <div>Clue-Less Login!</div>
+            <button onClick={this.alertMe.bind(this)}>click me</button>
         </div>);
     }
   
