754c706b0de6b1d6e2a04b1d389c97ec8de56afb
canhtrinh <canh.coding@gmail.com>
2017-11-19 16:18:22 -0500

    #6 basic form validation for login

--- Client/src/components/Login_Page/Login.js
+++ Client/src/components/Login_Page/Login.js
@@ -1,82 +1,128 @@
 import React from 'react';
 import './Login.css';
-import net from "net";
 import Settings from '../../Settings';
+import { Button, Form, FormGroup, Label, Input,
+    Modal, ModalHeader, ModalBody, ModalFooter
+} from 'reactstrap';
+import Messages from '../../classes/Messages';
+import ServerProxy from '../../classes/ServerProxy';
 
 export default class Login extends React.Component {
 
     constructor() {
         super();
         this.customServer = this.customServer.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
         this.state = {
-            showCustomServer: false
+            showCustomServer: false,
+            modal: false
         }
+    }
+    
+    componentDidMount() {
+        this.resetIpToDefault();       
+    }
+    
+    resetIpToDefault() {
+        this.ip = Settings.serverBaseUrl;
+        this.port = Settings.serverPort;
+        this.setState({showCustomServer: false});      
     }
 
     customServer() {
+
         const { showCustomServer } = this.state;
+
         return showCustomServer 
-            ? <div className="form-group">
-                <label className="ip-address-input">Server IP Address</label>
-                <input className="form-control" id="ip-address-input" type="text" placeholder="Enter an IP address" defaultValue="http://" />
-            </div>
+            ? <FormGroup>
+                <Label for="custom-ip">Enter a Custom IP address and port</Label>
+                <Input type="text" name="custom-ip" id="custom-ip" innerRef={node => this.customIp = node} placeholder="Default :: localhost:11000" />
+            </FormGroup>
             : null;
+
     }
 
-    customizeServerToggle(){
+    customizeServerToggle() {
+
         const { showCustomServer } = this.state;
+
         return showCustomServer 
             ? <div className="text-center">
-                <div className="d-block small mt-3 pointer-cursor" onClick={() => this.setState({showCustomServer: false})}>Login to standard server instead</div>
+                <div className="d-block small mt-3 pointer-cursor" onClick={this.resetIpToDefault.bind(this)}>Login to standard server instead</div>
             </div>
             : <div className="text-center">
-                <div className="d-block small mt-3 pointer-cursor" onClick={() => this.setState({showCustomServer: true})}>Login to a specific server (You must know the IP address)</div>
+                <div className="d-block small mt-3 pointer-cursor" onClick={() => this.setState({showCustomServer: true})}>Login to a specific server (You must know the IP address and port)</div>
             </div>;
+
+    }
+
+    renderModal() {
+        return <Modal isOpen={this.state.modal} toggle={this.toggleModal} className={this.props.className}>
+            <ModalBody>Attempting to log in to remote server: {this.ip}:{this.port}...</ModalBody>
+        </Modal>;
     }
 
     loginPage() {
+
         return <div className="container">
             <div className="card card-login mx-auto mt-5">
                 <div className="card-header">Login</div>
                 <div className="card-body">
-                    <div className="form-group">
-                        <label className="username-input">Username</label>
-                        <input className="form-control" id="username-input" placeholder="Enter a username" />
-                    </div>
-                    {this.customServer()}
-                    {this.customizeServerToggle()}
-                    <hr/>
-                    <div className="btn btn-primary btn-block pointer-cursor" onClick={this.testLoginToServer.bind(this)}>Click me to test TCP connection</div>
+                    <Form onSubmit={this.handleSubmit.bind(this)}>
+                        <FormGroup>
+                            <Label for="username">Enter a username</Label>
+                            <Input type="text" name="username" id="username" innerRef={node => this.username = node} placeholder="Username" />
+                        </FormGroup>
+                        {this.customServer()}
+                        {this.customizeServerToggle()}
+                        <hr/>
+                        <Button className="btn btn-block pointer-cursor">Submit</Button>
+                    </Form>
                 </div>
             </div>
-      </div>;
+        </div>;
+
     }
 
-    // todo: this is just a test for now
-    testLoginToServer() {
-        tcpConnection = new net.Socket();
-        
-        tcpConnection.connect(Settings.serverPort, Settings.serverBaseUrl, function() {
-            console.log('Connected');
-            tcpConnection.write('Hello, server! Love, Client.');
+    toggleModal(bool) {
+
+        this.setState({
+            modal: bool
         });
-        
-        tcpConnection.on('data', function(data) {
-            console.log('Received: ' + data);
-        });
-        
-        tcpConnection.on('close', function() {
-            console.log('Connection closed');
-        });
+
+    }
+
+    handleSubmit(e) {
+
+        let customAddress;
+
+        if (this.customIp && this.customIp.value && this.customIp.value.length > 0) {
+            customAddress = ServerProxy.customAddressFromUser(this.customIp.value);
+            if (customAddress && customAddress.ip && customAddress.port) {
+                this.ip = customAddress.ip;
+                this.port = customAddress.port;
+            }
+        }
+
+        if (this.username && this.username.value && this.username.value.length > 0) {
+            console.log("Your username :: ", this.username.value);
+            this.toggleModal(true);
+            ServerProxy.loginToServer(this.username.value, this.ip, this.port, this.toggleModal);
+        } else {
+            alert("Please enter a valid username!");
+        }
+
+        e.preventDefault();
+    
     }
 
     render() {
+
         return (<div>
             {this.loginPage()}
+            {this.renderModal()}
         </div>);
+
     }
 
 }
-
-
-
