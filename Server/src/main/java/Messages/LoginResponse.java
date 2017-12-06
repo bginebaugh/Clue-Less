f0a8f15617b4edbd404fe833070c3bf04f830695
canhtrinh <canh.coding@gmail.com>
2017-12-05 22:42:12 -0500

    adding username to LoginResponse from server to client

--- Server/src/main/java/Messages/LoginResponse.java
+++ Server/src/main/java/Messages/LoginResponse.java
@@ -3,6 +3,7 @@ package Messages;
 public class LoginResponse extends MessageBase {
 	private boolean valid = false;
 	private String moreInfo = "";
+	private String username = "";
 
 	public boolean getValid() {
 		return valid;
@@ -18,5 +19,13 @@ public class LoginResponse extends MessageBase {
 
 	public void setMoreInfo(String info) {
 		moreInfo = info;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String name) {
+		username = name;
 	}
 }
