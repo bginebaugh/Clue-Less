bfd16656d8358f014497874fb82395cab08a2381
Ch315929 <christina.helberg23@gmail.com>
2017-12-07 17:40:20 -0500

    Added the intialization to the cell class

--- Server/src/main/java/Cell.java
+++ Server/src/main/java/Cell.java
@@ -19,30 +19,30 @@ public class Cell {
 	Cell Study = new cell(0, 0, "Study", false); 
 	Cell StudyHall = new cell(0, 1, "StudyHall", true); 
 	Cell Hall = new cell(0, 2, "Hall", false); 
-	Cell HallLounge = new cell( 0, 3, 'HallLounge', true);
-	Cell Lounge = new cell( 0, 4, 'Lounge', false);
-	Cell StudyLib = new cell(1,0, 'StudyLib', true);
-	Cell HallBr = new cell(1,2, 'HallBr' , true);
-	Cell LoungeBr = new cell(1,4, 'LoungeBr', true);
-	Cell Library = new cell(2,0, 'Library' , false);
-	Cell LibBr = new cell(2,1, 'LibBr', true);
-	Cell BillRoom = new cell(2,2, 'BillRoom', false);
-	Cell BrDr = new cell(2,3, ' BrDr' , true);
-	Cell DiningRoom = new cell(2,4, 'DiningRoom', false);
-	Cell LibCons = new cell(3,0, 'LibCons' , true);
-	Cell BrBr = new cell(3,2, 'BrBr' , true);
-	Cell DrK = new cell(3,4, 'DrK', true);
-	Cell Conservatory = new cell(4,0, 'Conservatory', false);
-	Cell ConsBr = new cell(4,1, 'ConsBr' , true);
-	Cell Ballroom = new cell(4,2, 'Ballroom', false);
-	Cell BrK = new cell(4,3, 'BrK', true);
-	Cell Kitchen = new cell(4,4, 'Kitchen', false);
+	Cell HallLounge = new cell( 0, 3, "HallLounge", true);
+	Cell Lounge = new cell( 0, 4, "Lounge", false);
+	Cell StudyLib = new cell(1,0, "StudyLib", true);
+	Cell HallBr = new cell(1,2, "HallBr" , true);
+	Cell LoungeBr = new cell(1,4, "LoungeBr", true);
+	Cell Library = new cell(2,0, "Library" , false);
+	Cell LibBr = new cell(2,1, "LibBr", true);
+	Cell BillRoom = new cell(2,2, "BillRoom", false);
+	Cell BrDr = new cell(2,3, "BrDr" , true);
+	Cell DiningRoom = new cell(2,4, "DiningRoom", false);
+	Cell LibCons = new cell(3,0, "LibCons" , true);
+	Cell BrBr = new cell(3,2, "BrBr" , true);
+	Cell DrK = new cell(3,4, "DrK", true);
+	Cell Conservatory = new cell(4,0, "Conservatory", false);
+	Cell ConsBr = new cell(4,1, "ConsBr" , true);
+	Cell Ballroom = new cell(4,2, "Ballroom", false);
+	Cell BrK = new cell(4,3, "BrK", true);
+	Cell Kitchen = new cell(4,4, "Kitchen", false);
 	
 	private arrayList<Cell> board = new ArrayList<>(Arrays.asList(Study,StudyHall, Hall, HallLounge, Lounge, StudyLib, null, HallBr, null, LoungeBr, Library,
 			LibBr, BillRoom, BrDr, DiningRoom, LibCons, null, BrBr, null, DrK,Conservatory, ConsBr, Ballroom,
 			BrK, Kitchen)); 
 	
-	public int[][] getNeighbors(x,y){ 
+	public int[][] getNeighbors(int x,int y){ 
 		//storing  coordinates of neighboring cells, initialized to -1,-1
 		 int neighbors [][] = { {-1,-1} ,{-1,-1}, {-1,-1}, {-1,-1} };
 		//check if a cell above current position exists
